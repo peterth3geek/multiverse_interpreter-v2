cc82667a4c8a4f4e2766ce472c90e3daefaccc59
github username <github email address>
2018-08-22 11:34:47 -0400

    a form that does nothing, and a button that does nothing

--- back_end/app/controllers/users_controller.rb
+++ back_end/app/controllers/users_controller.rb
@@ -8,14 +8,9 @@ class UsersController < ApplicationController
     render json: @users
   end
 
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
   # POST /users
   def create
-    @user = User.new(user_params)
+    @user = User.find_or_create_by(user_params)
 
     if @user.save
       render json: @user, status: :created, location: @user
--- front_end/index.js
+++ front_end/index.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', () =>{
   // console.log("Hello!");
 
   const baseAztroURL = `https://aztro.sameerkumar.website/`
+  const USERURL = 'http://localhost:3000/users'
 
-  function getReading(sign, day){
+  function getReading(sign){
+    const day = 'tomorrow'
     const searchURL = baseAztroURL + `?sign=${sign}&day=${day}`
     return fetch(searchURL, {
       method: 'POST'
@@ -17,11 +19,21 @@ document.addEventListener('DOMContentLoaded', () =>{
     return fetch(gifSearch).then(r => r.json())
   }
 
-  renderReading('leo', 'tomorrow')
+  function saveUser(userOBJ){
+    return fetch(USERURL, {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify(userOBJ)
+    })
+  }
 
-  function renderReading(sign, day){
-    getReading(sign, day).then(reading => {
-      const body = document.getElementById('document-body')
+  createForm()
+
+  function renderReading(sign){
+    getReading(sign).then(reading => {
+      const body = document.getElementById('reading-display')
+      body.innerText = ''
+
       const ul = document.createElement('ul')
       const color = document.createElement('li')
       const luckyNumber = document.createElement('li')
@@ -40,11 +52,50 @@ document.addEventListener('DOMContentLoaded', () =>{
         mood.innerText = reading.mood
         rundown.innerText = reading.description
         console.log(reading);
-        ul.append(image, color, luckyNumber, luckyTime, mood, rundown)
+        ul.append(image, color, luckyNumber, luckyTime, mood, rundown, compatibility)
         body.append(ul)
       })
 
     })
   }
+
+  function createForm(){
+    const formBar = document.getElementById('form-bar')
+    const form = document.createElement('form')
+    const usernameInput = document.createElement('input')
+    const signInput = document.createElement('input')
+    const submitBtn = document.createElement('button')
+
+    form.name = 'form'
+
+    usernameInput.type = 'TEXT'
+    usernameInput.name = 'username'
+    usernameInput.placeholder = 'girrafe barf'
+    form.appendChild(usernameInput)
+
+    signInput.type = 'TEXT'
+    signInput.name = 'sign'
+    signInput.placeholder = 'Leo bitches'
+    form.appendChild(signInput)
+
+    submitBtn.innerText = 'Submit Bitches'
+    submitBtn.addEventListener('click', handleSubmit)
+    form.appendChild(submitBtn)
+
+    formBar.appendChild(form)
+
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const formData = e.target.parentElement
+    const formOBJ = {
+      username: formData.username.value,
+      sign: formData.sign.value
+    }
+    renderReading(formOBJ.sign)
+    saveUser(formOBJ).then(console.log)
+  }
+
 
 })
