9a36dba07a82db9360a7a33b86ea38e4ac52b5df
github username <github email address>
2018-08-24 10:06:42 -0400

    welcome landing

--- front_end/index.js
+++ front_end/index.js
@@ -3,24 +3,25 @@ document.addEventListener('DOMContentLoaded', welcome)
 
   function welcome(){
     const displayArea = document.getElementById('reading-display')
-
+    displayArea.className = 'center row h-100 justify-content-center align-items-center'
     const welcomeCard = document.createElement('div')
-      welcomeCard.className - 'card'
+      welcomeCard.className = 'card card-header-1 form-group welcome-card marginal micro-padding'
     const welcomeHeader = document.createElement('div')
-      welcomeHeader.appendChild(welcomeCard)
-    const welcomBody = document.createElement('div')
+    const welcomeBody = document.createElement('div')
 
     const welcomePhrase = document.createElement('h2')
       welcomePhrase.innerText = 'Welcome to the Multiverse'
+      welcomeHeader.appendChild(welcomePhrase)
 
     const form = document.createElement('form')
-      form.classList.add('form-inline')
+      // form.className =
       form.name = 'form'
     const usernameInput = document.createElement('input')
-      usernameInput.type = 'TEXT'
+      usernameInput.type = 'text'
+      usernameInput.size = 50
       usernameInput.name = 'username'
-      usernameInput.placeholder = 'Your Username'
-      usernameInput.className = 'form-control mb-2 mr-sm-2 mb-sm-0'
+      usernameInput.placeholder = 'Create or Submit Your Username'
+      usernameInput.className = 'form-control-lg'
     const submitBtn = document.createElement('button')
       submitBtn.innerText = 'Get Multiverse Reading'
       submitBtn.addEventListener('click', Adapter.handleSubmitWelcome)
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', welcome)
     const signSelect = document.createElement('select')
       const zodiacSigns = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
       signSelect.name = 'sign'
-      signSelect.className = 'custom-select mb-2 mr-sm-2 mb-sm-0'
+      signSelect.className = 'custom-select form-control-lg mb-2 mr-sm-2 mb-sm-0'
       zodiacSigns.map(sign => signSelect.appendChild(new Option(sign, sign)));
     const readingsBtn = document.createElement('button')
       readingsBtn.innerText = 'All My Readings'
@@ -36,8 +37,8 @@ document.addEventListener('DOMContentLoaded', welcome)
       readingsBtn.className = 'btn btn-primary mr-sm-2'
 
     form.append(usernameInput, signSelect, submitBtn, readingsBtn)
-    welcomBody.appendChild(form)
-    welcomeCard.appendChild(welcomBody)
+    welcomeBody.appendChild(form)
+    welcomeCard.append(welcomeHeader, welcomeBody)
     displayArea.appendChild(welcomeCard)
 
     return createForm()
